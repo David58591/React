836993e839f13b8ca3931bdecceaf7faba4255ea
David Egea Lozano <david.egea.daw@gmail.com>
2020-12-27 19:59:30 +0100

    fin de Conversor de Celsium a Farenheit y viceversa

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Calculadora from './Calculadora';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Calculadora from "./Calculadora";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Calculadora />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <React.Fragment>
+    <Calculadora />,
+  </React.Fragment>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
